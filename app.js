e8caa1bb86f289ceb6e2c5cf062e0af50083c2e7
Jugnu Agrawal <jugnuagrawal.brjn@gmail.com>
2018-06-07 18:20:28 +0530

    removed _

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const host = process.env.HOST || '127.0.0.1';
 const port = process.env.PORT || config.port || 3000;
 const mongo_url = process.env.MONGO_URL || 'mongodb://localhost:27017/' + (config.database || 'maau');
 const secret = config.secret;
-const _model = mongoose.model('user');
+const model = mongoose.model('user');
 
 //log4js configuration
 log4js.configure({
@@ -27,57 +27,57 @@ app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 //logging each request
-app.use(function (_req, _res, _next) {
-    var ip = _req.headers['x-forwarded-for'] || _req.connection.remoteAddress;
-    logger.info(_req.method, ip, _req.path, _req.params, _req.query, _req.body);
-    _res.set('Access-Control-Allow-Origin','*');
-    _res.set('Access-Control-Allow-Methods','*');
-    _res.set('Access-Control-Allow-Headers','content-type,authorization');
-    if(_req.method == 'OPTIONS'){
-        _res.status(204).end();
+app.use(function (req, res, next) {
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    logger.info(req.method, ip, req.path, req.params, req.query, req.body);
+    res.set('Access-Control-Allow-Origin','*');
+    res.set('Access-Control-Allow-Methods','*');
+    res.set('Access-Control-Allow-Headers','content-type,authorization');
+    if(req.method == 'OPTIONS'){
+        res.status(204).end();
     }else{
-        _next();
+        next();
     }
 });
 
 //checking mongodb is available
-app.use(function (_req, _res, _next) {
+app.use(function (req, res, next) {
     if (mongoose.connections.length == 0 || mongoose.connections[0].readyState != 1) {
         mongoose.connect(mongo_url, function (_err) {
             if (_err) {
                 logger.error(_err);
-                _res.status(500).json({ message: messages['500'] });
+                res.status(500).json({ message: messages['500'] });
             } else {
-                _next();
+                next();
             }
         });
     } else {
-        _next();
+        next();
     }
 });
 
 // Uncomment and right your own business logic to do Authentication check
-app.use(function (_req, _res, _next) {
-    if (_req.path != '/apidoc' && _req.path != '/activate' && _req.path != '/login' && _req.path != '/register' && _req.path != '/forgot') {
-        if (_req.headers.authorization) {
-            jwt.verify(_req.headers.authorization, secret, (err, decoded) => {
+app.use(function (req, res, next) {
+    if (req.path != '/apidoc' && req.path != '/activate' && req.path != '/login' && req.path != '/register' && req.path != '/forgot') {
+        if (req.headers.authorization) {
+            jwt.verify(req.headers.authorization, secret, (err, decoded) => {
                 if (err || !decoded) {
-                    _res.status(401).json({ message: messages['401'] });
+                    res.status(401).json({ message: messages['401'] });
                     return;
                 }
-                _model.findOne({ email: decoded.email }, (_err, _data) => {
+                model.findOne({ email: decoded.email }, (_err, _data) => {
                     if (_err || !_data) {
-                        _res.status(404).json({ message: messages['401'] });
+                        res.status(404).json({ message: messages['401'] });
                         return;
                     }
-                    _next();
+                    next();
                 });
             });
         } else {
-            _res.status(401).json({ message: messages['401'] });
+            res.status(401).json({ message: messages['401'] });
         }
     } else {
-        _next();
+        next();
     }
 });
 
@@ -97,8 +97,8 @@ app.get('/apidoc', (req, res) => {
 });
 
 //Invalid routes handle
-app.use('*', function (_req, _res) {
-    _res.status(404).json({ message: messages['404'] });
+app.use('*', function (req, res) {
+    res.status(404).json({ message: messages['404'] });
 });
 
 
@@ -111,7 +111,7 @@ app.listen(port, host, function () {
     defaultUser.createdAt = new Date();
     defaultUser.lastUpdated = new Date();
     defaultUser.deleted = false;
-    _model.create(defaultUser).then(data => {
+    model.create(defaultUser).then(data => {
         logger.info(data);
     }).catch(err => {
         logger.error(err);
